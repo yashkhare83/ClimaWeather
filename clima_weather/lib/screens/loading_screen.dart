ccaf01b5e2a62ff1c89c31f999130d6a736f7361
Yash Khare <yashk@dewsolutions.in>
2023-05-03 18:15:54 +0530

    add geolocator, async await and future

--- clima_weather/lib/screens/loading_screen.dart
+++ clima_weather/lib/screens/loading_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -11,25 +15,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
 
-    print(position);
+  void getLocation() async {
+    Location location = Location();
+    // ignore: await_only_futures
+    await location.getCurrentLocation();
+    print(location.latitute);
+    print(location.longitude);
+  }
+
+  Future<void> getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=kztfqd1prnQXWHsMWjrGhBGbnrGEfvPi'));
+
+    if (response.statusCode == 200) {
+      String fetchData = response.body;
+      var res = jsonDecode(fetchData)['data']['timelines'][0]['timestep'];
+      print(res);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+        // body: Center(
+        //   child: ElevatedButton(
+        //     onPressed: () {
+        //       //Get the current location
+        //       getLocation();
+        //     },
+        //     child: Text('Get Location'),
+        //   ),
+        // ),
+        );
   }
 }
